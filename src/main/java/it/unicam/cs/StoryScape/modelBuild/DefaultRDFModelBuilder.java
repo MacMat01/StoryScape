88472f2d727deaff3e4cb3f7fe9ce6817192c92d
Matteo Machella <matteo.machella01@gmail.com>
2023-06-30 12:00:28 +0200

    RDFModelBuilder implemented

---
+++ src/main/java/it/unicam/cs/StoryScape/modelBuild/DefaultRDFModelBuilder.java
@@ -0,0 +1,41 @@
+package it.unicam.cs.StoryScape.modelBuild;
+
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.util.FileManager;
+
+import java.util.Objects;
+
+/**
+ * Class that represents the default RDF model builder.
+ */
+public class DefaultRDFModelBuilder implements RDFModelBuilder {
+
+    private final FileManager fileManager = FileManager.getInternal();
+
+    @Override
+    public Model buildRDFModel() {
+        return ModelFactory.createDefaultModel();
+    }
+
+    @Override
+    public Model buildRDFModel(String path) {
+        Model model = ModelFactory.createDefaultModel();
+        RDFDataMgr.read(model, Objects.requireNonNull(fileManager.open(path)), null);
+        return model;
+    }
+
+    @Override
+    public Model buildOntologyModel(OntModelSpec modelSpec) {
+        return ModelFactory.createOntologyModel(modelSpec);
+    }
+
+    @Override
+    public Model buildOntologyModel(String path, OntModelSpec modelSpec) {
+        Model model = ModelFactory.createOntologyModel(modelSpec);
+        RDFDataMgr.read(model, Objects.requireNonNull(fileManager.open(path)), null);
+        return model;
+    }
+}
---
+++ src/main/java/it/unicam/cs/StoryScape/modelBuild/InferredRDFModelBuilder.java
@@ -0,0 +1,43 @@
+package it.unicam.cs.StoryScape.modelBuild;
+
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.reasoner.Reasoner;
+
+/**
+ * Class that represents the inferred RDF model builder.
+ */
+public class InferredRDFModelBuilder extends DefaultRDFModelBuilder {
+
+    /**
+     * Creates a new inferred RDF model.
+     *
+     * @param model    the model to be inferred
+     * @param reasoner the reasoner to be used
+     * @return the inferred model
+     */
+    public InfModel buildInferredModel(InfModel model, Reasoner reasoner) {
+//        reasoner = reasoner.bindSchema(model);
+        return ModelFactory.createInfModel(reasoner, model);
+    }
+
+    /**
+     * Checks if the model is consistent.
+     *
+     * @return true if the model is consistent, false otherwise.
+     */
+    public boolean isConsistent(InfModel model) {
+        return model.validate().isValid();
+    }
+
+    /**
+     * Checks if the inference is valid.
+     *
+     * @return true if the inference is valid, false otherwise.
+     */
+    public boolean isValidInference(InfModel model, Resource subject, Property predicate, Resource object) {
+        return model.contains(subject, predicate, object);
+    }
+}
