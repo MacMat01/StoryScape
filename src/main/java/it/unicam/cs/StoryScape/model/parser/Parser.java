b207f31265192e7be3b0c51b9da5eb4974ea6725
Matteo Machella <matteo.machella01@gmail.com>
2023-06-30 17:03:32 +0200

    Parser interfaces implemented

---
+++ src/main/java/it/unicam/cs/StoryScape/model/parser/Parser.java
@@ -0,0 +1,17 @@
+package it.unicam.cs.StoryScape.model.parser;
+
+import org.apache.jena.query.QueryExecution;
+
+/**
+ * Interface that represents the parser wich parses the SPARQL query .
+ */
+public interface Parser {
+
+    /**
+     * Method that parses the SPARQL query.
+     *
+     * @param query the SPARQL query
+     * @return the query execution
+     */
+    ParsedData parse(QueryExecution query);
+}
