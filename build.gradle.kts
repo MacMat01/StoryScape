f647a3c6f04de0674eb31fb8c6f6c29590f4121e
Matteo Machella <matteo.machella01@gmail.com>
2023-06-30 09:58:16 +0200

    Imported Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,16 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.apache.jena:jena-core:4.3.0")
+
+    // https://mvnrepository.com/artifact/org.apache.jena/jena-core
+    implementation("org.apache.jena:jena-core:4.8.0")
+    implementation("org.apache.jena:jena-arq:4.8.0")
+
+    // https://github.com/Galigator/openllet
+    implementation("com.github.galigator.openllet:openllet-jena:2.6.5")
+    implementation("com.github.galigator.openllet:openllet-owlapi:2.6.5")
 }
 
 tasks.test {
