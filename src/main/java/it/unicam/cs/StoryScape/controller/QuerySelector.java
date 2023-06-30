1bbc4ee1ce2154b9dbc95cc5b77d96dfb57cc322
Matteo Machella <matteo.machella01@gmail.com>
2023-06-30 19:32:05 +0200

    Implemented QuerySelector class

---
+++ src/main/java/it/unicam/cs/StoryScape/controller/QuerySelector.java
@@ -0,0 +1,42 @@
+package it.unicam.cs.StoryScape.controller;
+
+import it.unicam.cs.StoryScape.model.SPARQL.SelectionQueries;
+import it.unicam.cs.StoryScape.model.parser.ParsedData;
+
+/**
+ * Class that represents the query selector.
+ */
+public class QuerySelector {
+
+    private final Controller controller = new Controller();
+
+    /**
+     * Returns the list of books in the ontology.
+     *
+     * @return the list of books
+     */
+    public ParsedData getBooks() {
+        return controller.getData(SelectionQueries.BOOK_LIST);
+    }
+
+    /**
+     * Returns the details of a book.
+     *
+     * @param book the book
+     * @return the details of the book
+     */
+    public ParsedData getBookDetails(String book) {
+        return controller.getData(SelectionQueries.BOOK_DETAILS, book);
+    }
+
+    /**
+     * Returns the list of books that start with the given string.
+     *
+     * @param book the book
+     * @return the list of books that start with the given string
+     */
+    public ParsedData searchBook(String book) {
+        return controller.getData(SelectionQueries.SEARCH_BOOK, book);
+    }
+
+}
