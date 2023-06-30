4064153bc64a304d34f35cb9d7292faf7723e6b1
Matteo Machella <matteo.machella01@gmail.com>
2023-06-30 17:54:09 +0200

    Implemented JSONParser and the record JSONData

---
+++ src/main/java/it/unicam/cs/StoryScape/model/parser/JSONData.java
@@ -0,0 +1,50 @@
+package it.unicam.cs.StoryScape.model.parser;
+
+import com.google.gson.Gson;
+
+import java.util.Collection;
+
+public record JSONData(String jsonData) implements ParsedData {
+
+    public static JSONData fromObject(Object object) {
+        Gson gson = new Gson();
+        String jsonData = gson.toJson(object);
+        return new JSONData(jsonData);
+    }
+
+    @Override
+    public String getProperty(String property) {
+        Gson gson = new Gson();
+        DataObject dataObject = gson.fromJson(jsonData, DataObject.class);
+        if (dataObject != null) {
+            return dataObject.getProperty(property);
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<String> getValues() {
+        Gson gson = new Gson();
+        DataObject dataObject = gson.fromJson(jsonData, DataObject.class);
+        if (dataObject != null) {
+            return dataObject.getValues();
+        }
+        return null;
+    }
+
+    /**
+     * Class that represents the DataObject used to parse the JSON data.
+     */
+    private static class DataObject {
+        private String property;
+        private Collection<String> values;
+
+        public String getProperty(String property) {
+            return this.property;
+        }
+
+        public Collection<String> getValues() {
+            return this.values;
+        }
+    }
+}
---
+++ src/main/java/it/unicam/cs/StoryScape/model/parser/JSONParser.java
@@ -0,0 +1,60 @@
+package it.unicam.cs.StoryScape.model.parser;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.RDFNode;
+
+/**
+ * Class that represents the JSON parser.
+ */
+public class JSONParser implements Parser {
+
+    /**
+     * Method that parses a query result node to a string.
+     *
+     * @param queryExecution the SPARQL query
+     * @return the parsed data as JSONData
+     */
+    public JSONData parse(QueryExecution queryExecution) {
+        JsonObject jsonData = new JsonObject();
+
+        ResultSet results = queryExecution.execSelect();
+        while (results.hasNext()) {
+            QuerySolution result = results.nextSolution();
+            String label = parseNodeToString(result.get("label"));
+            String value = parseNodeToString(result.get("value"));
+
+            // Concatenates multiple values of the same property
+            if (jsonData.has(label)) {
+                jsonData.addProperty(label, jsonData.get(label).getAsString() + ", " + value);
+            } else {
+                jsonData.addProperty(label, value);
+            }
+        }
+
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(jsonData);
+        return new JSONData(jsonString);
+    }
+
+    /**
+     * Converts a Jena RDF node to a string.
+     *
+     * @param node the RDF node
+     * @return the string representation of the node
+     */
+    private String parseNodeToString(RDFNode node) {
+        if (node == null) {
+            return null;
+        } else if (node.isLiteral()) {
+            return node.asLiteral().getString();
+        } else if (node.isResource()) {
+            return node.asResource().getURI();
+        } else {
+            return null;
+        }
+    }
+}
