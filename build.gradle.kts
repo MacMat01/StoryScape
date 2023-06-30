413bd3849b4a416ac2acaceec2d33a2cfc5f9fde
Matteo Machella <matteo.machella01@gmail.com>
2023-06-30 17:53:27 +0200

    Added GSON dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 
     // https://mvnrepository.com/artifact/org.apache.jena/
     implementation("org.apache.jena:jena-core:4.8.0")
@@ -20,6 +20,10 @@ dependencies {
     // https://github.com/Galigator/openllet
     implementation("com.github.galigator.openllet:openllet-jena:2.6.5")
     implementation("com.github.galigator.openllet:openllet-owlapi:2.6.5")
+
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
 }
 
 tasks.test {
