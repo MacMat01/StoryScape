db0f4dd0e0a04280c568beba6bdc2078620f0bab
Matteo Machella <matteo.machella01@gmail.com>
2023-07-01 17:54:03 +0200

    Implemented App, AppController and main.fxml

---
+++ src/main/java/it/unicam/cs/storyscape/App.java
@@ -0,0 +1,22 @@
+package it.unicam.cs.storyscape;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main class of the application.
+ */
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+            // TODO
+
+    }
+}
